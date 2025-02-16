9461f17f63efefed12b0ad25e0df50a85d9b61a8
dhanushshettigar <dhanushshettigar90@gmail.com>
2025-02-16 21:24:38 +0530

    SLAM Auto Config

--- launch/slam_toolbox_online_async.launch.py
+++ launch/slam_toolbox_online_async.launch.py
@@ -1,10 +1,20 @@
 import os
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import DeclareLaunchArgument, TimerAction, EmitEvent
 from launch.substitutions import LaunchConfiguration
-from launch_ros.actions import Node
+from launch_ros.actions import LifecycleNode
+from launch_ros.events.lifecycle import ChangeState
+from launch_ros.events.lifecycle import matches_node_name
 from ament_index_python.packages import get_package_share_directory
+
+########################
+#    Modifications     #
+########################
+#ros2 service call /slam_toolbox/change_state lifecycle_msgs/srv/ChangeState "{transition: {id: 1}}"
+#ros2 service call /slam_toolbox/change_state lifecycle_msgs/srv/ChangeState "{transition: {id: 3}}"
+#ros2 service call /slam_toolbox/get_state lifecycle_msgs/srv/GetState "{}"
+
 
 
 def generate_launch_description():
@@ -13,24 +23,36 @@ def generate_launch_description():
     declare_slam_params_file_cmd = DeclareLaunchArgument(
         'slam_params_file',
         default_value=os.path.join(get_package_share_directory("tcebot_slam"),
-                                   'config', 'slam_toolbox_online_async.yaml'),
+                                   'config', 'slam_toolbox_async.yaml'),
         description='Full path to the ROS 2 parameters file to use for the slam_toolbox node')
 
-    start_async_slam_toolbox_node = Node(
-        parameters=[
-          slam_params_file,
-          {
-            'use_sim_time': True
-          }
-        ],
+    start_async_slam_toolbox_node = LifecycleNode(
         package='slam_toolbox',
         executable='async_slam_toolbox_node',
         name='slam_toolbox',
-        output='screen')
+        namespace='',
+        output='screen',
+        parameters=[slam_params_file, {'use_sim_time': True, 'autostart': True}],
+    )
 
-    ld = LaunchDescription()
+    # Emit events to transition SLAM toolbox to configured and then active states
+    configure_slam = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=matches_node_name('/slam_toolbox'),
+            transition_id=1,
+        )
+    )
 
-    ld.add_action(declare_slam_params_file_cmd)
-    ld.add_action(start_async_slam_toolbox_node)
+    activate_slam = EmitEvent(
+        event=ChangeState(
+            lifecycle_node_matcher=matches_node_name('/slam_toolbox'),
+            transition_id=3,  
+        )
+    )
 
-    return ld
+    return LaunchDescription([
+        declare_slam_params_file_cmd,
+        start_async_slam_toolbox_node,
+        TimerAction(period=2.0, actions=[configure_slam]),
+        TimerAction(period=4.0, actions=[activate_slam]),
+    ])
